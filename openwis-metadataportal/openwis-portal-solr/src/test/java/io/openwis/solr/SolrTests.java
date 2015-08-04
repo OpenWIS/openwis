590816a0524bb6bbf7b01ee0bb45dc139e7d47dd
Martin Gollogly <martin.gollogly@scisys.co.uk>
2015-08-04 16:43:24 +0100

    Added More SOLR Unit Tests

--- openwis-metadataportal/openwis-portal-solr/src/test/java/io/openwis/solr/SolrTests.java
+++ openwis-metadataportal/openwis-portal-solr/src/test/java/io/openwis/solr/SolrTests.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.request.SolrPing;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.params.SolrParams;
@@ -19,11 +20,9 @@ import org.testng.annotations.Test;
 
 public class SolrTests extends SolrTestBase {
 
-	
 	public SolrTests() throws Exception {
 		super();
 	}
-
 
 	@Test
 	public void testThatNoResultsAreReturned() throws SolrServerException {
@@ -32,7 +31,6 @@ public class SolrTests extends SolrTestBase {
 
 		assertEquals(0L, response.getResults().getNumFound());
 	}
-	
 
 	@Test
 	public void testThatDocumentIsInserted() throws SolrServerException,
@@ -47,14 +45,14 @@ public class SolrTests extends SolrTestBase {
 
 		SolrParams params = new SolrQuery("_uuid:a*");
 		QueryResponse response = server.query(params);
-		
+
 		assertEquals(1L, response.getResults().getNumFound());
 		assertEquals("abc", response.getResults().get(0).get("_uuid"));
 	}
-	
+
 	@Test
-	public void testThatDocumentIsDeleted()
-			throws SolrServerException, IOException {
+	public void testThatDocumentIsDeleted() throws SolrServerException,
+			IOException {
 
 		SolrInputDocument document = new SolrInputDocument();
 		document.addField("_uuid", "def");
@@ -63,22 +61,42 @@ public class SolrTests extends SolrTestBase {
 
 		server.add(document);
 		server.commit();
-		
+
 		SolrParams params = new SolrQuery("_uuid:def");
 		QueryResponse response = server.query(params);
-		
+
 		assertEquals(1L, response.getResults().getNumFound());
 		assertEquals("def", response.getResults().get(0).get("_uuid"));
-		
+
 		server.deleteByQuery("_uuid:def");
 		server.commit();
-		
+
 		QueryResponse responsePostDelete = server.query(params);
-		
+
 		assertEquals(0L, responsePostDelete.getStatus());
 		assertEquals(0L, responsePostDelete.getResults().getNumFound());
-		
+
 	}
-	
+
+	@Test
+	public void testPingHandler() throws Exception {
+
+		// Empty the database...
+		server.deleteByQuery("*:*");// delete everything!
+		server.commit();
+		assertNumFound("*:*", 0); // make sure it got in
+
+		// should be ok
+		server.ping();
+
+		try {
+			SolrPing ping = new SolrPing();
+			ping.getParams().set("qt", "unknown handler!");
+			ping.process(server);
+			fail("sent unknown query type!");
+		} catch (Exception ex) {
+			// expected
+		}
+	}
 
 }
