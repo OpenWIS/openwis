5b848678ca663413d915eb9b485f835c250161de
Leon Mika <l.mika@bom.gov.au>
2016-01-25 15:05:27 +1100

    Added the GetSelf user service
    This service will only return the current logged in user, regardless of
    what is in the request.  This was implemented as a subclass of Get,
    which contains all the unmarshalling code.
    
    Also removed the password field from the user profile form.

--- openwis-metadataportal/openwis-portal/src/main/webapp/scripts/Openwis/lib/Openwis/Common/User/UserInformation.js
+++ openwis-metadataportal/openwis-portal/src/main/webapp/scripts/Openwis/lib/Openwis/Common/User/UserInformation.js
@@ -15,10 +15,9 @@ Openwis.Common.User.UserInformation = Ext.extend(Ext.Container, {
 	
 	getInfosAndInitialize: function() {
 		var params = {};
-		params.editingPersoInfo = "true";
 		
 		var getHandler = new Openwis.Handler.Get({
-			url: configOptions.locService+ '/xml.user.get',
+			url: configOptions.locService+ '/xml.user.getSelf',
 			params: params,
 			listeners: {
 				success: function(config) {
@@ -86,7 +85,10 @@ Openwis.Common.User.UserInformation = Ext.extend(Ext.Container, {
 	
 	getPersonalInformationFormPanel: function() {
         if(!this.personalInformationFormPanel) {
-            this.personalInformationFormPanel = new Openwis.Common.User.PersonalInformation({isEdition: 'true'});
+            this.personalInformationFormPanel = new Openwis.Common.User.PersonalInformation({
+            	hidePassword: this.hidePassword,
+            	isEdition: 'true'
+    		});
         }
         return this.personalInformationFormPanel;
     },
