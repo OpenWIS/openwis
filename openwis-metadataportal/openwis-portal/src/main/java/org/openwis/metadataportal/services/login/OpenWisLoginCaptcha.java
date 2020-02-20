4353a1d754772e030c46b87ad07c93647667f17a
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2020-02-20 14:56:31 +0100

    implement verification of google recaptcha

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/login/OpenWisLoginCaptcha.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/login/OpenWisLoginCaptcha.java
@@ -1,10 +1,15 @@
 package org.openwis.metadataportal.services.login;
 
-import jeeves.server.context.ServiceContext;
 import jeeves.utils.Log;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.json.JSONObject;
+import org.openwis.metadataportal.common.configuration.ConfigurationConstants;
+import org.openwis.metadataportal.common.configuration.OpenwisMetadataPortalConfig;
 import org.openwis.metadataportal.services.util.OpenWISMessages;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class OpenWisLoginCaptcha extends HttpServlet {
+
+    // Url for verifying client captcha response
+    private final String GOOGLE_CAPTCHA_VERIFICATION_URL="https://www.google.com/recaptcha/api/siteverify";
+
+    private final String GOOGLE_CAPTCHA_PARAMETER_RESPONSE="g-recaptcha-response";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
@@ -36,11 +46,30 @@ public class OpenWisLoginCaptcha extends HttpServlet {
     {
 
         try {
-            String userCaptchaResponse = request.getParameter("jcaptcha");
-            //Check whether the captcha passed or not
-            boolean captchaPassed = OpenWisImageCaptchaServlet.validateResponse(request, userCaptchaResponse);
+            boolean captchaPassed = false;
+            HttpClient client = new HttpClient();
+            PostMethod method = new PostMethod(GOOGLE_CAPTCHA_VERIFICATION_URL);
+            // set headers
+            method.setRequestHeader("Content-Type","application/json");
+            method.setRequestHeader("Accept", "application/json");
 
-            //If captcha passed, send mail to end user
+            NameValuePair[] queryParam = {
+                    new NameValuePair("secret", OpenwisMetadataPortalConfig
+                            .getString(ConfigurationConstants.GOOGLE_RECAPTCHA_SECRET_KEY)),
+                    new NameValuePair("response", request.getParameter(GOOGLE_CAPTCHA_PARAMETER_RESPONSE))
+            };
+            method.setQueryString(queryParam);
+
+            int statusCode = client.executeMethod(method);
+            if (statusCode == HttpStatus.SC_OK) {
+                // Read the response body.
+                byte[] responseBody = method.getResponseBody();
+
+                JSONObject json = new JSONObject(new String(responseBody));
+                // Deal with the response.
+                captchaPassed = json.getBoolean("success");
+            }
+
             if (captchaPassed)
             {
                 String[] uris=request.getRequestURI().split("/");
