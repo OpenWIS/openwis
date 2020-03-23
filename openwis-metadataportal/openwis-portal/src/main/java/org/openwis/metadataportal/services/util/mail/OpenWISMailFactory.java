a7c6e0c1492193bee60e47f27a9694db315769be
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-03-23 06:26:03 +0100

    add methods for the rest of the templates

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/util/mail/OpenWISMailFactory.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/util/mail/OpenWISMailFactory.java
@@ -15,8 +15,7 @@ public class OpenWISMailFactory {
      * @return OpenWISMail
      */
     public static OpenWISMail buildRecoverAccountUserMail(ServiceContext context, String subjectVar, String[] destinations, Map<String,Object> contentVars) {
-        EmailTemplate emailTemplate = new EmailTemplate(context.getAppPath(), EmailTemplate.RECOVER_ACCOUNT_TEMPLATE);
-        return new OpenWISMail(context, subjectVar, destinations, emailTemplate, contentVars);
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.RECOVER_ACCOUNT_TEMPLATE, contentVars);
     }
 
     /**
@@ -27,7 +26,86 @@ public class OpenWISMailFactory {
      * @return OpenWISMail
      */
     public static OpenWISMail buildRecoverAccountAdminMail(ServiceContext context, String subjectVar, Map<String, Object> contentVars) {
-        EmailTemplate emailTemplate = new EmailTemplate(context.getAppPath(), EmailTemplate.RECOVER_ADMIN_ACCOUNT_TEMPLATE);
+        return buildAdminMail(context, subjectVar, EmailTemplate.RECOVER_ADMIN_ACCOUNT_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new request account mail
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param destinations destination addresses
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildRequestAccountUserMail(ServiceContext context, String subjectVar, String[] destinations, Map<String, Object> contentVars) {
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.REQUEST_ACCOUNT_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new request account mail to be sent to administrator
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildRequestAccountAdminMail(ServiceContext context, String subjectVar,  Map<String, Object> contentVars) {
+        return buildAdminMail(context, subjectVar, EmailTemplate.REQUEST_ADMIN_ACCOUNT_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new account disabled mail
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param destinations destination addresses
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildAccountDisabledMail(ServiceContext context, String subjectVar, String[] destinations, Map<String, Object> contentVars) {
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.ACCOUNT_DISABLED_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new account termination mail
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param destinations destination addresses
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildAccountTerminationMail(ServiceContext context, String subjectVar, String[] destinations, Map<String, Object> contentVars) {
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.ACCOUNT_TERMINATION_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new account creation mail
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param destinations destination addresses
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildAccountCreationMail(ServiceContext context, String subjectVar, String[] destinations, Map<String, Object> contentVars) {
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.ACCOUNT_CREATION_TEMPLATE, contentVars);
+    }
+
+    /**
+     * Return a new password expire notification mail
+     * @param context service context
+     * @param subjectVar subject property key
+     * @param destinations destination addresses
+     * @param contentVars body data
+     * @return OpenWISMail
+     */
+    public static OpenWISMail buildPasswordExpireNotificationMail(ServiceContext context, String subjectVar, String[] destinations, Map<String, Object> contentVars) {
+        return buildUserMail(context, subjectVar, destinations, EmailTemplate.PASSWORD_EXPIRE_NOTIFICATION_TEMPLATE, contentVars);
+    }
+    private static OpenWISMail buildUserMail(ServiceContext context, String subjectVar, String[] destinations, String templateName, Map<String, Object> contentVars) {
+        EmailTemplate emailTemplate = new EmailTemplate(context.getAppPath(), templateName);
+        return new OpenWISMail(context, subjectVar, emailTemplate, contentVars);
+    }
+
+    private static OpenWISMail buildAdminMail(ServiceContext context, String subjectVar,  String templateName, Map<String, Object> contentVars) {
+        EmailTemplate emailTemplate = new EmailTemplate(context.getAppPath(), templateName);
         return new OpenWISMail(context, subjectVar, emailTemplate, contentVars);
     }
 }
