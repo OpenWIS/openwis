3adb0fd4032ee41e41adf421d340a9d5e7611901
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-03-26 07:13:26 +0100

    factory for account tasks

---
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/AccountTaskFactory.java
@@ -0,0 +1,41 @@
+package org.openwis.metadataportal.kernel.scheduler;
+
+import jeeves.resources.dbms.Dbms;
+import jeeves.server.context.ServiceContext;
+import org.openwis.metadataportal.services.util.mail.IOpenWISMail;
+import org.openwis.metadataportal.services.util.mail.OpenWISMailFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class AccountTaskFactory {
+
+    public static AccountTask buildAccountLockTask(ServiceContext context, Dbms dbms, Long duration, TimeUnit timeUnit) {
+        AccountFilter filter = new LastLoginFilter(duration, timeUnit);
+
+        Map<String, Object> mailContent = new HashMap<>();
+        mailContent.put("duration",duration);
+        mailContent.put("timeUnit", timeUnit.toString());
+
+        // Create a partial mail without destination. Destination address will be set be the task for each user.
+        IOpenWISMail mail = OpenWISMailFactory.buildAccountTerminationMail(context, "subject", null,mailContent);
+
+        AccountAction accountAction = new AccountLockAction(dbms, filter, mail);
+        return new AccountTask(accountAction);
+    }
+
+    public static AccountTask buildAccountTerminationNotificationTask(ServiceContext context, Dbms dbms, Long duration, TimeUnit timeUnit) {
+        AccountFilter filter = new LastLoginFilter(duration, timeUnit);
+
+        Map<String, Object> mailContent = new HashMap<>();
+        mailContent.put("duration",duration);
+        mailContent.put("timeUnit", timeUnit.toString());
+
+        // Create a partial mail without destination. Destination address will be set be the task for each user.
+        IOpenWISMail mail = OpenWISMailFactory.buildAccountDisabledMail(context, "subject", null,mailContent);
+
+        AccountAction accountAction = new AccountLockingNotificationAction(dbms, filter, mail);
+        return new AccountTask(accountAction);
+    }
+}
