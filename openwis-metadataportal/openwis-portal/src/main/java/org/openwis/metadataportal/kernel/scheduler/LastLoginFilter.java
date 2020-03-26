74d066f3082ec23c5cf7c03aa779147d5f121669
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-03-26 11:24:20 +0100

    fix time compare between threshold and last login date

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/LastLoginFilter.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/LastLoginFilter.java
@@ -2,10 +2,12 @@ package org.openwis.metadataportal.kernel.scheduler;
 
 import jeeves.utils.Log;
 import org.openwis.metadataportal.model.user.User;
-import org.openwis.metadataportal.services.login.LoginConstants;
 
-import java.sql.Timestamp;
-import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -17,33 +19,42 @@ import java.util.concurrent.TimeUnit;
 public class LastLoginFilter implements AccountFilter {
 
     // Represents the duration in timeUnits from the last login date for which the user passe the filter
-    private final Long duration;
+    private final Integer duration;
 
     // time unit
     private final TimeUnit timeUnit;
 
-    public LastLoginFilter(Long duration, TimeUnit timeUnit) {
+    public LastLoginFilter(Integer duration, TimeUnit timeUnit) {
         this.duration = duration;
         this.timeUnit = timeUnit;
     }
+
     @Override
     public List<User> filter(List<User> users) {
         List<User> filteredUsers = new ArrayList<>();
-        long now = Instant.now().toEpochMilli();
-        long threshold = now - this.timeUnit.toMillis(this.duration);
+
+        // compute threshold
+        OffsetDateTime now = LocalDateTime.now().atOffset(ZoneOffset.UTC);
+        OffsetDateTime threshold = now.minus(duration, ChronoUnit.valueOf(timeUnit.toString()));
+        Log.debug(Log.SCHEDULER, String.format("Threshold is: %s", threshold.format(DateTimeFormatter.ISO_DATE_TIME)));
 
         // filtered users
-        for (User user: users) {
-            if (this.timestampToMillis(user.getLastLogin()) > threshold) {
-                Log.debug(LoginConstants.LAST_LOGIN_FILTER, "Last login of user " + user.getUsername() + " is greater than threshold." );
+        for (User user : users) {
+            if (user.getProfile().toLowerCase().equals("admin") || user.getProfile().toLowerCase().equals("administrator")) {
+                continue;
+            }
+
+            Log.debug(Log.SCHEDULER, String.format("User %s. Last login: %s", user.getUsername(), user.getLastLogin().toLocalDateTime().atOffset(ZoneOffset.UTC)));
+
+            if (user.getLastLogin().toLocalDateTime().atOffset(ZoneOffset.UTC).isBefore(threshold)) {
+                Log.debug(Log.SCHEDULER, String.format("User: %s. Last login: %s. Threshold: %s",
+                        user.getUsername(),
+                        user.getLastLoginAsString(),
+                        threshold.toString()));
                 filteredUsers.add(user);
             }
         }
 
         return filteredUsers;
-    }
-
-    private long timestampToMillis(Timestamp timestamp) {
-        return TimeUnit.MILLISECONDS.convert(timestamp.getNanos(), TimeUnit.NANOSECONDS);
     }
 }
