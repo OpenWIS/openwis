2e63d186b6387b950ded00e3eea2ca56fb4a8468
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-03-31 09:35:23 +0200

    format to 4 spaces

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/model/user/User.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/model/user/User.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.openwis.metadataportal.model.user;
 
@@ -26,337 +26,337 @@ import java.util.List;
  */
 public class User {
 
-   /**
-    * The id of the user.
-    */
-   private Integer id;
-   
-   /**
-    * @member: name The user name
-    */
-   private String name;
+    /**
+     * The id of the user.
+     */
+    private Integer id;
 
-   /**
-    * @member: userName The user username
-    */
-   private String username;
+    /**
+     * @member: name The user name
+     */
+    private String name;
 
-   /**
-    * @member: surName The user surname
-    */
-   private String surname;
+    /**
+     * @member: userName The user username
+     */
+    private String username;
 
-   /**
-    * @member: password The user password
-    */
-   private String password;
+    /**
+     * @member: surName The user surname
+     */
+    private String surname;
 
-   /**
-    * @member: emailContact The user email contact
-    */
-   private String emailContact;
+    /**
+     * @member: password The user password
+     */
+    private String password;
 
-   /**
-    * @member: emails List of emails (favourites) for dissemination parameters
-    */
-   private List<OpenWISEmail> emails = new ArrayList<OpenWISEmail>();
+    /**
+     * @member: emailContact The user email contact
+     */
+    private String emailContact;
 
-   /**
-    * @member: ftps List of ftps (favourites) for dissemination parameters
-    */
-   private List<OpenWISFTP> ftps = new ArrayList<OpenWISFTP>();
+    /**
+     * @member: emails List of emails (favourites) for dissemination parameters
+     */
+    private List<OpenWISEmail> emails = new ArrayList<OpenWISEmail>();
 
-   /**
-    * @member: address The user address (address, zip, state, city, country)
-    */
-   private Address address;
+    /**
+     * @member: ftps List of ftps (favourites) for dissemination parameters
+     */
+    private List<OpenWISFTP> ftps = new ArrayList<OpenWISFTP>();
 
-   /**
-    * @member: needUserAccount True if the user need to have a
-    * local account to access to a centre.
-    */
-   private boolean needUserAccount;
+    /**
+     * @member: address The user address (address, zip, state, city, country)
+     */
+    private Address address;
 
-   /**
-    * @member: profile The user profile : Admin / Operator / Editor
-    */
-   private String profile;
+    /**
+     * @member: needUserAccount True if the user need to have a
+     * local account to access to a centre.
+     */
+    private boolean needUserAccount;
 
-   /**
-    * The class Of Service : Gold, Silver or Bronze.
-    * @member: classOfService
-    */
-   private ClassOfService classOfService;
+    /**
+     * @member: profile The user profile : Admin / Operator / Editor
+     */
+    private String profile;
 
-   /**
-    * List of deployments on which the user can be backed up.
-    * @member: backUps
-    */
-   private List<BackUp> backUps = new ArrayList<BackUp>();
+    /**
+     * The class Of Service : Gold, Silver or Bronze.
+     * @member: classOfService
+     */
+    private ClassOfService classOfService;
 
-   /**
-    * @member: groups The user's groups.
-    */
-   private List<Group> groups =  new ArrayList<Group>();
+    /**
+     * List of deployments on which the user can be backed up.
+     * @member: backUps
+     */
+    private List<BackUp> backUps = new ArrayList<BackUp>();
 
-   /**
-    * last login time
-    */
-   private Timestamp lastLogin;
+    /**
+     * @member: groups The user's groups.
+     */
+    private List<Group> groups =  new ArrayList<Group>();
 
-   /**
-    * Account status: Active or Inactive
-    */
-   private InetUserStatus inetUserStatus;
+    /**
+     * last login time
+     */
+    private Timestamp lastLogin;
 
-   /**
-    * Gets the id.
-    * @return the id.
-    */
-   public Integer getId() {
-      return id;
-   }
+    /**
+     * Account status: Active or Inactive
+     */
+    private InetUserStatus inetUserStatus;
 
-   /**
-    * Sets the id.
-    * @param id the id to set.
-    */
-   public void setId(Integer id) {
-      this.id = id;
-   }
+    /**
+     * Gets the id.
+     * @return the id.
+     */
+    public Integer getId() {
+        return id;
+    }
 
-   /**
-    * Gets the name.
-    * @return the name.
-    */
-   public String getName() {
-      return name;
-   }
+    /**
+     * Sets the id.
+     * @param id the id to set.
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-   /**
-    * Sets the name.
-    * @param name the name to set.
-    */
-   public void setName(String name) {
-      this.name = name;
-   }
+    /**
+     * Gets the name.
+     * @return the name.
+     */
+    public String getName() {
+        return name;
+    }
 
-   /**
-    * Gets the username.
-    * @return the username.
-    */
-   public String getUsername() {
-      return username;
-   }
+    /**
+     * Sets the name.
+     * @param name the name to set.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-   /**
-    * Sets the username.
-    * @param username the userName to set.
-    */
-   public void setUsername(String userName) {
-      this.username = userName;
-   }
+    /**
+     * Gets the username.
+     * @return the username.
+     */
+    public String getUsername() {
+        return username;
+    }
 
-   /**
-    * Gets the surName.
-    * @return the surName.
-    */
-   public String getSurname() {
-      return surname;
-   }
+    /**
+     * Sets the username.
+     * @param username the userName to set.
+     */
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
 
-   /**
-    * Sets the surName.
-    * @param surName the surName to set.
-    */
-   public void setSurname(String surName) {
-      this.surname = surName;
-   }
+    /**
+     * Gets the surName.
+     * @return the surName.
+     */
+    public String getSurname() {
+        return surname;
+    }
 
-   /**
-    * Gets the password.
-    * @return the password.
-    */
-   public String getPassword() {
-      return password;
-   }
+    /**
+     * Sets the surName.
+     * @param surName the surName to set.
+     */
+    public void setSurname(String surName) {
+        this.surname = surName;
+    }
 
-   /**
-    * Sets the password.
-    * @param password the password to set.
-    */
-   public void setPassword(String password) {
-      this.password = password;
-   }
+    /**
+     * Gets the password.
+     * @return the password.
+     */
+    public String getPassword() {
+        return password;
+    }
 
-   /**
-    * Gets the emailContact.
-    * @return the emailContact.
-    */
-   public String getEmailContact() {
-      return emailContact;
-   }
+    /**
+     * Sets the password.
+     * @param password the password to set.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-   /**
-    * Sets the emailContact.
-    * @param emailContact the emailContact to set.
-    */
-   public void setEmailContact(String emailContact) {
-      this.emailContact = emailContact;
-   }
+    /**
+     * Gets the emailContact.
+     * @return the emailContact.
+     */
+    public String getEmailContact() {
+        return emailContact;
+    }
 
-   /**
-    * Gets the emails.
-    * @return the emails.
-    */
-   public List<OpenWISEmail> getEmails() {
-      return emails;
-   }
+    /**
+     * Sets the emailContact.
+     * @param emailContact the emailContact to set.
+     */
+    public void setEmailContact(String emailContact) {
+        this.emailContact = emailContact;
+    }
 
-   /**
-    * Sets the emails.
-    * @param emails the emails to set.
-    */
-   public void setEmails(List<OpenWISEmail> emails) {
-      this.emails = emails;
-   }
+    /**
+     * Gets the emails.
+     * @return the emails.
+     */
+    public List<OpenWISEmail> getEmails() {
+        return emails;
+    }
 
-   /**
-    * Gets the ftps.
-    * @return the ftps.
-    */
-   public List<OpenWISFTP> getFtps() {
-      return ftps;
-   }
+    /**
+     * Sets the emails.
+     * @param emails the emails to set.
+     */
+    public void setEmails(List<OpenWISEmail> emails) {
+        this.emails = emails;
+    }
 
-   /**
-    * Sets the ftps.
-    * @param ftps the ftps to set.
-    */
-   public void setFtps(List<OpenWISFTP> ftps) {
-      this.ftps = ftps;
-   }
+    /**
+     * Gets the ftps.
+     * @return the ftps.
+     */
+    public List<OpenWISFTP> getFtps() {
+        return ftps;
+    }
 
-   /**
-    * Gets the address.
-    * @return the address.
-    */
-   public Address getAddress() {
-      return address;
-   }
+    /**
+     * Sets the ftps.
+     * @param ftps the ftps to set.
+     */
+    public void setFtps(List<OpenWISFTP> ftps) {
+        this.ftps = ftps;
+    }
 
-   /**
-    * Sets the address.
-    * @param address the address to set.
-    */
-   public void setAddress(Address address) {
-      this.address = address;
-   }
+    /**
+     * Gets the address.
+     * @return the address.
+     */
+    public Address getAddress() {
+        return address;
+    }
 
-   /**
-    * Gets the needUserAccount.
-    * @return the needUserAccount.
-    */
-   public boolean isNeedUserAccount() {
-      return needUserAccount;
-   }
+    /**
+     * Sets the address.
+     * @param address the address to set.
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
-   /**
-    * Sets the needUserAccount.
-    * @param needUserAccount the needUserAccount to set.
-    */
-   public void setNeedUserAccount(boolean needUserAccount) {
-      this.needUserAccount = needUserAccount;
-   }
+    /**
+     * Gets the needUserAccount.
+     * @return the needUserAccount.
+     */
+    public boolean isNeedUserAccount() {
+        return needUserAccount;
+    }
 
-   /**
-    * Gets the profile.
-    * @return the profile.
-    */
-   public String getProfile() {
-      return profile;
-   }
+    /**
+     * Sets the needUserAccount.
+     * @param needUserAccount the needUserAccount to set.
+     */
+    public void setNeedUserAccount(boolean needUserAccount) {
+        this.needUserAccount = needUserAccount;
+    }
 
-   /**
-    * Sets the profile.
-    * @param profile the profile to set.
-    */
-   public void setProfile(String profile) {
-      this.profile = profile;
-   }
+    /**
+     * Gets the profile.
+     * @return the profile.
+     */
+    public String getProfile() {
+        return profile;
+    }
 
-   /**
-    * Gets the classOfService.
-    * @return the classOfService.
-    */
-   public ClassOfService getClassOfService() {
-      return classOfService;
-   }
+    /**
+     * Sets the profile.
+     * @param profile the profile to set.
+     */
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
 
-   /**
-    * Sets the classOfService.
-    * @param classOfService the classOfService to set.
-    */
-   public void setClassOfService(ClassOfService classOfService) {
-      this.classOfService = classOfService;
-   }
+    /**
+     * Gets the classOfService.
+     * @return the classOfService.
+     */
+    public ClassOfService getClassOfService() {
+        return classOfService;
+    }
 
-   /**
-    * Gets the backUps.
-    * @return the backUps.
-    */
-   public List<BackUp> getBackUps() {
-      return backUps;
-   }
+    /**
+     * Sets the classOfService.
+     * @param classOfService the classOfService to set.
+     */
+    public void setClassOfService(ClassOfService classOfService) {
+        this.classOfService = classOfService;
+    }
 
-   /**
-    * Sets the backUps.
-    * @param backUps the backUps to set.
-    */
-   public void setBackUps(List<BackUp> backUps) {
-      this.backUps = backUps;
-   }
+    /**
+     * Gets the backUps.
+     * @return the backUps.
+     */
+    public List<BackUp> getBackUps() {
+        return backUps;
+    }
 
-   /**
-    * Gets the groups.
-    * @return the groups.
-    */
-   public List<Group> getGroups() {
-      return groups;
-   }
+    /**
+     * Sets the backUps.
+     * @param backUps the backUps to set.
+     */
+    public void setBackUps(List<BackUp> backUps) {
+        this.backUps = backUps;
+    }
 
-   /**
-    * Sets the groups.
-    * @param groups the groups to set.
-    */
-   public void setGroups(List<Group> groups) {
-      this.groups = groups;
-   }
+    /**
+     * Gets the groups.
+     * @return the groups.
+     */
+    public List<Group> getGroups() {
+        return groups;
+    }
 
-   @JsonIgnore
-   public Timestamp getLastLogin() {
-      return lastLogin;
-   }
+    /**
+     * Sets the groups.
+     * @param groups the groups to set.
+     */
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
 
-   public void setLastLogin(Timestamp lastLogin) {
-      this.lastLogin = lastLogin;
-   }
+    @JsonIgnore
+    public Timestamp getLastLogin() {
+        return lastLogin;
+    }
 
-   @JsonProperty("lastLogin")
-   public String getLastLoginAsString() {
-      if (lastLogin == null) {
-         return "";
-      }
-      ZoneId zoneId = ZoneId.systemDefault();
-      ZonedDateTime zdt = ZonedDateTime.ofInstant(this.lastLogin.toInstant(), zoneId);
-      return zdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
-   }
+    public void setLastLogin(Timestamp lastLogin) {
+        this.lastLogin = lastLogin;
+    }
 
-   public InetUserStatus getInetUserStatus() {
-      return inetUserStatus;
-   }
+    @JsonProperty("lastLogin")
+    public String getLastLoginAsString() {
+        if (lastLogin == null) {
+            return "";
+        }
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(this.lastLogin.toInstant(), zoneId);
+        return zdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL));
+    }
 
-   public void setInetUserStatus(InetUserStatus inetUserStatus) {
-      this.inetUserStatus = inetUserStatus;
-   }
+    public InetUserStatus getInetUserStatus() {
+        return inetUserStatus;
+    }
+
+    public void setInetUserStatus(InetUserStatus inetUserStatus) {
+        this.inetUserStatus = inetUserStatus;
+    }
 }
