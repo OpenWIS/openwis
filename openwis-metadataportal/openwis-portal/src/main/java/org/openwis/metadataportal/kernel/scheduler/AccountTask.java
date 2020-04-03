24a87108408e366eba79de60a770f45fb1a9e635
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-04-03 11:16:38 +0200

    mad task class
    the task class will filter the users based on a list of filters and
    for each user will run the actions provided in the constructor.

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/AccountTask.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/AccountTask.java
@@ -1,17 +1,49 @@
 package org.openwis.metadataportal.kernel.scheduler;
 
+import jeeves.utils.Log;
+import org.openwis.metadataportal.kernel.scheduler.actions.AccountAction;
+import org.openwis.metadataportal.kernel.scheduler.filters.AccountFilter;
+import org.openwis.metadataportal.kernel.user.UserManager;
+import org.openwis.metadataportal.model.user.User;
+import org.openwis.securityservice.UserManagementException_Exception;
+import java.util.List;
+
 /**
  * This task do some action on user accounts
  */
 public class AccountTask implements Runnable {
 
-    private final AccountAction action;
+    private String name;
+    private final UserManager userManager;
+    private final List<AccountFilter> filters;
+    private final List<AccountAction> actions;
 
-    public AccountTask(AccountAction action) {
-        this.action = action;
+    public AccountTask(String name, UserManager userManager, List<AccountFilter> filters, List<AccountAction> actions) {
+        this.name = name;
+        this.userManager = userManager;
+        this.filters = filters;
+        this.actions = actions;
     }
+
     @Override
     public void run() {
-       this.action.doAction();
+        Log.info(Log.SCHEDULER, String.format("=============== %s ===============", this.name));
+
+        try {
+            List<User> filteredUsers = userManager.getAllUsers();
+            for (AccountFilter filter : filters) {
+                filteredUsers = filter.filter(filteredUsers);
+            }
+
+            Log.info(Log.SCHEDULER, String.format("Found %d users", filteredUsers.size()));
+            for (User user : filteredUsers) {
+                for (AccountAction action : actions) {
+                    action.doAction(user);
+                }
+            }
+            Log.info(Log.SCHEDULER, String.format("=============== Finished %s ===============", this.name));
+        } catch (UserManagementException_Exception ex) {
+            Log.error(Log.SCHEDULER, ex);
+        }
     }
 }
--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/AccountTaskFactory.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/AccountTaskFactory.java
@@ -3,51 +3,92 @@ package org.openwis.metadataportal.kernel.scheduler;
 import jeeves.resources.dbms.Dbms;
 import jeeves.server.context.ServiceContext;
 import org.openwis.management.alert.AlertService;
+import org.openwis.metadataportal.kernel.scheduler.actions.*;
 import org.openwis.metadataportal.kernel.scheduler.filters.*;
+import org.openwis.metadataportal.kernel.user.UserManager;
+import org.openwis.metadataportal.services.user.dto.UserAction;
 import org.openwis.metadataportal.services.util.mail.IOpenWISMail;
 import org.openwis.metadataportal.services.util.mail.OpenWISMailFactory;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class AccountTaskFactory {
 
     public static AccountTask buildAccountLockTask(ServiceContext context, Dbms dbms, AlertService alertService, Integer duration, TimeUnit timeUnit) {
-        AccountFilter[] filters = new AccountFilter[]{
+        UserManager userManager = new UserManager(dbms);
+        // Create filters
+        List<AccountFilter> filters = Arrays.asList(
                 new ProfileFilter("user"),
                 new ActiveAccountFilter(),
                 new NotifiedUserFilter(dbms),
                 new LastLoginFilter(duration, timeUnit)
-        };
+        );
 
+        List<AccountAction> actions = new ArrayList<>();
+        // lock action
+        actions.add(new AccountLockAction(userManager));
+        // create mail action
         Map<String, Object> mailContent = new HashMap<>();
         mailContent.put("duration",duration);
         mailContent.put("timeUnit", timeUnit.toString().toLowerCase());
-
-        // Create a partial mail without destination. Destination address will be set be the task for each user.
         IOpenWISMail mail = OpenWISMailFactory.buildAccountTerminationMail(context, "Account.lock.subject", null,mailContent);
+        actions.add(new MailAction(mail));
 
-        AccountAction accountAction = new AccountLockAction(dbms, alertService, filters, mail);
-        return new AccountTask(accountAction);
+        //log action
+        actions.add(new LogAction(dbms, UserAction.LOCK));
+
+        // alert action
+        actions.add(new AlertAction(alertService, UserAction.LOCK));
+        return new AccountTask("Account Lock Task", userManager, filters, actions);
     }
 
     public static AccountTask buildAccountActivityNotificationTask(ServiceContext context, Dbms dbms, AlertService alertService, Integer duration, TimeUnit timeUnit) {
-        AccountFilter[] filters = new AccountFilter[]{
+        UserManager userManager = new UserManager(dbms);
+        List<AccountFilter> filters = Arrays.asList(
                 new ProfileFilter("user"),
                 new ActiveAccountFilter(),
                 new UnnotifiedUserFilter(dbms),
                 new LastLoginFilter(duration, timeUnit)
-        };
+        );
 
+        List<AccountAction> actions = new ArrayList<>();
         Map<String, Object> mailContent = new HashMap<>();
         mailContent.put("duration",duration);
         mailContent.put("timeUnit", timeUnit.toString().toLowerCase());
-
-        // Create a partial mail without destination. Destination address will be set be the task for each user.
         IOpenWISMail mail = OpenWISMailFactory.buildAccountDisabledMail(context, "Account.notification.subject", null,mailContent);
+        actions.add(new MailAction(mail));
 
-        AccountAction accountAction = new AccountActivityNotificationAction(dbms, alertService, filters, mail);
-        return new AccountTask(accountAction);
+        // log action
+        actions.add(new LogAction(dbms, UserAction.INACTIVITY_NOTIFICATION_MAIL));
+
+        //alert action
+        actions.add(new AlertAction(alertService, UserAction.INACTIVITY_NOTIFICATION_MAIL));
+
+        return new AccountTask("Account Inactivity Task", userManager, filters, actions);
+    }
+
+    public static AccountTask buildPasswordExpireNotificationTask(ServiceContext context, Dbms dbms, AlertService alertService, Integer duration, TimeUnit timeUnit) {
+        UserManager userManager = new UserManager(dbms);
+        List<AccountFilter> filters = Arrays.asList(
+                new ProfileFilter("user"),
+                new ActiveAccountFilter(),
+                new PasswordNotificationFilter(dbms)
+        );
+
+        List<AccountAction> actions = new ArrayList<>();
+        Map<String, Object> mailContent = new HashMap<>();
+        mailContent.put("duration",duration);
+        mailContent.put("timeUnit", timeUnit.toString().toLowerCase());
+        IOpenWISMail mail = OpenWISMailFactory.buildPasswordExpireNotificationMail(context, "Password.notification.subject", null,mailContent);
+        actions.add(new PasswordNotificationMailAction(mail));
+
+        // log action
+        actions.add(new LogAction(dbms, UserAction.PASSWORD_EXPIRE_NOTIFICATION_MAIL));
+
+        //alert action
+        actions.add(new AlertAction(alertService, UserAction.PASSWORD_EXPIRE_NOTIFICATION_MAIL));
+
+        return new AccountTask("Password expire notification task", userManager, filters, actions);
     }
 }
