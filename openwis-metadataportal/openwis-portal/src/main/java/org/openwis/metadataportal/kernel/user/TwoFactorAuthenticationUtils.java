6b74972c06bb53457a3f39160f1fdbbdc6240d55
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-04-06 17:55:48 +0200

    fix encoding/deconding hex and google authenticator url

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/user/TwoFactorAuthenticationUtils.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/user/TwoFactorAuthenticationUtils.java
@@ -1,31 +1,35 @@
 package org.openwis.metadataportal.kernel.user;
 
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang.RandomStringUtils;
 
-import java.nio.charset.Charset;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 /**
  * Created by cosmin on 23/07/19.
  */
 public class TwoFactorAuthenticationUtils {
 
-    private final static int KEY_LENGTH = 12;
-
     /**
+     * Encoding in base16 is needed by OpenAM
      * Encode {@param key} to Base16
      * @param key secret key
      * @return secret key encoded to Base16
      */
     public static String encodeBase16(String key) {
-        char[] encoded = Base16.encode(key.getBytes(), false);
-        return new String(encoded);
+        return Hex.encodeHexString(key.getBytes());
     }
 
     public static String decodeBase16(String key) {
-        byte[] bytes = Base16.decode(key.toCharArray());
-        return new String(bytes, StandardCharsets.UTF_8);
+        try {
+            return new String(Hex.decodeHex(key.toCharArray()));
+        } catch (DecoderException e) {
+            return "";
+        }
     }
 
     /**
@@ -38,14 +42,22 @@ public class TwoFactorAuthenticationUtils {
         return base32.encodeAsString(key.getBytes());
     }
 
+    public static String decodeBase32(String key) {
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(key);
+        return new String(bytes);
+    }
+
     /**
-     * Generate a secret key
+     * Generate a secret key encoded in base16
      * @return secret key
      */
     public static String generateKey() {
-        byte[] array = new byte[KEY_LENGTH];
-        new Random().nextBytes(array);
-        return new String(array, Charset.forName("UTF-8"));
+        int length = 10;
+        boolean useLetters = true;
+        boolean useNumbers = false;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+        return Hex.encodeHexString(generatedString.getBytes());
     }
 
     /**
@@ -55,11 +67,15 @@ public class TwoFactorAuthenticationUtils {
      * @param secretKey the secret key encoded in Base32
      * @return uri
      */
-    public static String getTOPTKeyUri(String userEmail, String secretKey) {
-        StringBuilder uriBuilder = new StringBuilder("otpauth://totp/OpenWIS:");
-        uriBuilder.append(userEmail);
-        uriBuilder.append("?secret=").append(secretKey);
-        uriBuilder.append("&issuer=OpenWIS&period=30");
-        return uriBuilder.toString();
+    public static String getGoogleAuthenticatorBarCode(String userEmail, String secretKey) throws IllegalArgumentException {
+        try {
+            String issuer = "OpenWIS";
+            return "otpauth://totp/"
+                    + URLEncoder.encode(issuer + ":" + userEmail, "UTF-8").replace("+", "%20")
+                    + "?secret=" + URLEncoder.encode(secretKey, "UTF-8").replace("+", "%20")
+                    + "&issuer=" + URLEncoder.encode(issuer, "UTF-8").replace("+", "%20");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
