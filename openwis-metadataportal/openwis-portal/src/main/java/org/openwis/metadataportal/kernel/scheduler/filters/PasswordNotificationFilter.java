ec54427ec7e9f014f0f0ba3b72113b117b8e3b92
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-04-16 14:25:23 +0200

    fix class name passed to log

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/filters/PasswordNotificationFilter.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/kernel/scheduler/filters/PasswordNotificationFilter.java
@@ -32,13 +32,13 @@ public class PasswordNotificationFilter implements AccountFilter {
                 UserLogDTO lastNotificationLog = logFilter.getLastLogEntry(logs,user, UserAction.PASSWORD_EXPIRE_NOTIFICATION_MAIL);
                 if (lastNotificationLog == null) {
                     Log.debug(Log.SCHEDULER, String.format("%s: Found user not notified: %s. User is passing the filter.",
-                            NotNotifiedUserFilter.class.getSimpleName(),
+                            PasswordNotificationFilter.class.getSimpleName(),
                             user.getUsername()));
                     filteredUsers.add(user);
                 } else {
                     if (user.getPwdChangedTime().isAfter(lastNotificationLog.getDate())) {
                         Log.debug(Log.SCHEDULER, String.format("%s: Found user not notified: %s. Last notification was: %s.",
-                                NotNotifiedUserFilter.class.getSimpleName(),
+                                PasswordNotificationFilter.class.getSimpleName(),
                                 user.getUsername(),
                                 lastNotificationLog.getDate().toString()));
                         filteredUsers.add(user);
