0fd95acb9b19b9440f4be08e309a3e3ea8c81c69
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-05-20 09:53:12 +0200

    get the last login from user_log table instead of users table.

--- openwis-metadataportal/openwis-portal/src/main/java/org/fao/geonet/constants/Geonet.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/fao/geonet/constants/Geonet.java
@@ -94,6 +94,7 @@ public class Geonet
 		public static final String SUMMARY    = "summary";
 		public static final String SITE_URL   = "siteURL";
 		public static final String APP_PATH   = "path";
+		public static final String DATE = "date";
 	}
 
 	//--------------------------------------------------------------------------
--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/access/Get.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/access/Get.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.openwis.metadataportal.services.access;
 
@@ -10,6 +10,7 @@ import jeeves.resources.dbms.Dbms;
 import jeeves.server.ProfileManager;
 import jeeves.server.ServiceConfig;
 import jeeves.server.context.ServiceContext;
+import jeeves.utils.Log;
 import org.apache.commons.collections.CollectionUtils;
 import org.fao.geonet.constants.Geonet;
 import org.jdom.Element;
@@ -22,8 +23,10 @@ import org.openwis.metadataportal.kernel.external.HarnessProvider;
 import org.openwis.metadataportal.services.mock.MockGetDisseminationParameters;
 import org.openwis.metadataportal.services.mock.MockMode;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -37,103 +40,118 @@ import java.util.Map;
  * Get all services list + add service if MSS FSS service is allowed.
  */
 public class Get implements Service, ServiceWithJsp {
-   
-   /**
-    * True if the portal is the portal user.
-    * @member: isUserPortal
-    */
-   private boolean isUserPortal;
 
-   /**
-    * {@inheritDoc}
-    * @see jeeves.interfaces.Service#init(java.lang.String, jeeves.server.ServiceConfig)
-    */
-   public void init(String appPath, ServiceConfig params) throws Exception {
-      isUserPortal = "user".equals(params.getValue("portal"));
-   }
+    /**
+     * True if the portal is the portal user.
+     * @member: isUserPortal
+     */
+    private boolean isUserPortal;
 
-   //--------------------------------------------------------------------------
-   //---
-   //--- Service
-   //---
-   //--------------------------------------------------------------------------
+    /**
+     * {@inheritDoc}
+     * @see jeeves.interfaces.Service#init(java.lang.String, jeeves.server.ServiceConfig)
+     */
+    public void init(String appPath, ServiceConfig params) throws Exception {
+        isUserPortal = "user".equals(params.getValue("portal"));
+    }
 
-   /**
-    * {@inheritDoc}
-    * @see jeeves.interfaces.Service#exec(org.jdom.Element, jeeves.server.context.ServiceContext)
-    */
-   public Element exec(Element params, ServiceContext context) throws Exception {
-      String profile = ProfileManager.GUEST;
+    //--------------------------------------------------------------------------
+    //---
+    //--- Service
+    //---
+    //--------------------------------------------------------------------------
 
-      if (context.getUserSession().isAuthenticated()) {
-         profile = context.getUserSession().getProfile();
-      }
-      
-      Element servicesElement = context.getProfileManager().getAccessibleServices(profile);
-      
-      if (isUserPortal) {
-       //Compute operations allowed.
-         boolean isMssFssSupported = OpenwisMetadataPortalConfig
-               .getBoolean(ConfigurationConstants.MSSFSS_SUPPORT);
-         List<String> channels = null;
-         if (isMssFssSupported) {
-            if (MockMode.isMockModeHarnessMSSFSS()) {
-               channels = MockGetDisseminationParameters.getMSSFSSChannelsMock();
-            } else {
-               MSSFSS mssFssService = HarnessProvider.getMSSFSSService();
-               channels = mssFssService.getChannelsForUser(context.getUserSession().getUsername());
+    /**
+     * {@inheritDoc}
+     * @see jeeves.interfaces.Service#exec(org.jdom.Element, jeeves.server.context.ServiceContext)
+     */
+    public Element exec(Element params, ServiceContext context) throws Exception {
+        String profile = ProfileManager.GUEST;
+
+        if (context.getUserSession().isAuthenticated()) {
+            profile = context.getUserSession().getProfile();
+        }
+
+        Element servicesElement = context.getProfileManager().getAccessibleServices(profile);
+
+        if (isUserPortal) {
+            //Compute operations allowed.
+            boolean isMssFssSupported = OpenwisMetadataPortalConfig
+                    .getBoolean(ConfigurationConstants.MSSFSS_SUPPORT);
+            List<String> channels = null;
+            if (isMssFssSupported) {
+                if (MockMode.isMockModeHarnessMSSFSS()) {
+                    channels = MockGetDisseminationParameters.getMSSFSSChannelsMock();
+                } else {
+                    MSSFSS mssFssService = HarnessProvider.getMSSFSSService();
+                    channels = mssFssService.getChannelsForUser(context.getUserSession().getUsername());
+                }
             }
-         }
 
-         boolean isAllowedMSSFSS = isMssFssSupported && CollectionUtils.isNotEmpty(channels);
-         if (isAllowedMSSFSS) {
-         // Add allowedMSSFSS service to the service list.
-            Element allowedMSSFSS = new Element(Jeeves.Elem.SERVICE);
-            allowedMSSFSS.setAttribute(Jeeves.Attr.NAME, "allowedMSSFSS");
-            servicesElement.addContent(allowedMSSFSS);
-         }
-      }
-         
-      return servicesElement;
-   }
-   
-   public Map<String, Object> execWithJsp(Element params, ServiceContext context) throws Exception {
-      Element servicesElement = exec(params, context);
-      ArrayList<String> services = new ArrayList<String>();
-      for (Object element : servicesElement.getChildren()) {
-         Element el = (Element) element;
-         String value = el.getAttributeValue(Jeeves.Attr.NAME);
-         services.add(value);
-      }
-      
-      Map<String, Object> attrMap = new HashMap<String, Object>();
-      attrMap.put("availableServices", services);
-      
-      if (isUserPortal) {
-         Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
-         IAvailabilityManager availabilityManager = new AvailabilityManager(dbms);
-         boolean isUserPortalEnabled = availabilityManager.isUserPortalEnable();
-         attrMap.put("isUserPortalEnabled", isUserPortalEnabled);
-
-         // get last login time
-         if (context.getUserSession().isAuthenticated()) {
-            // attempt to load user from db.
-            String username = context.getUserSession().getUsername();
-            String query = "SELECT * FROM Users WHERE username = ?";
-
-            @SuppressWarnings("unchecked")
-            List<Element> list = dbms.select(query, username).getChildren();
-            Element user = (Element) list.get(0);
-
-            String sLastLogin = user.getChildText(Geonet.Elem.LAST_LOGIN);
-            if (!sLastLogin.isEmpty()) {
-               ZoneId zoneId = ZoneId.systemDefault();
-               ZonedDateTime zdt = LocalDateTime.parse(sLastLogin).atZone(zoneId);
-               attrMap.put("userLastLogin", zdt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL)));
+            boolean isAllowedMSSFSS = isMssFssSupported && CollectionUtils.isNotEmpty(channels);
+            if (isAllowedMSSFSS) {
+                // Add allowedMSSFSS service to the service list.
+                Element allowedMSSFSS = new Element(Jeeves.Elem.SERVICE);
+                allowedMSSFSS.setAttribute(Jeeves.Attr.NAME, "allowedMSSFSS");
+                servicesElement.addContent(allowedMSSFSS);
             }
-         }
-      }
-      
-      return attrMap;
-   }
+        }
+
+        return servicesElement;
+    }
+
+    public Map<String, Object> execWithJsp(Element params, ServiceContext context) throws Exception {
+        Element servicesElement = exec(params, context);
+        ArrayList<String> services = new ArrayList<String>();
+        for (Object element : servicesElement.getChildren()) {
+            Element el = (Element) element;
+            String value = el.getAttributeValue(Jeeves.Attr.NAME);
+            services.add(value);
+        }
+
+        Map<String, Object> attrMap = new HashMap<String, Object>();
+        attrMap.put("availableServices", services);
+
+        if (isUserPortal) {
+            Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
+            IAvailabilityManager availabilityManager = new AvailabilityManager(dbms);
+            boolean isUserPortalEnabled = availabilityManager.isUserPortalEnable();
+            attrMap.put("isUserPortalEnabled", isUserPortalEnabled);
+
+            // get last login time
+            if (context.getUserSession().isAuthenticated()) {
+                try {
+                    // attempt to load user from db.
+                    String username = context.getUserSession().getUsername();
+                    LocalDateTime lastLogin = getLastLogin(dbms, username);
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    attrMap.put("userLastLogin", ZonedDateTime.of(lastLogin, ZoneOffset.UTC).format(formatter));
+                } catch (SQLException ex) {
+                    Log.error(Log.JEEVES, "Last login query error: " + ex.getMessage());
+                }
+            }
+        }
+
+        return attrMap;
+    }
+
+    /**
+     * Return the last login of the authenticated user.
+     * The last login is considered the login date before the current login.
+     * @param username username
+     * @return last login date or the current date if there are no login records from this user
+     */
+    private LocalDateTime getLastLogin(Dbms dbms, String username) throws SQLException {
+        String query = "SELECT * FROM user_log WHERE username = ? AND action = ? ORDER BY ? DESC LIMIT ?";
+        List<Element> list = dbms.select(query, username,"LOGIN",Geonet.Elem.DATE, 2).getChildren();
+
+        // we should have at least one entry. The second entry is the one which we want
+        if (list.size() == 2) {
+            Element entry = (Element) list.get(1);
+            String sLastLogin = entry.getChildText(Geonet.Elem.DATE);
+            return LocalDateTime.parse(sLastLogin,DateTimeFormatter.ISO_DATE_TIME);
+        } else {
+            return LocalDateTime.now();
+        }
+    }
 }
