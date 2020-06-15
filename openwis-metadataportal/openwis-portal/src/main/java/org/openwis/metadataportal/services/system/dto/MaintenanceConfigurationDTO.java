4ba9ac53433aaab300d830efd5c1d2f66709688c
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2020-06-15 08:44:23 +0200

    change startDate and endDate type

--- openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/system/dto/MaintenanceConfigurationDTO.java
+++ openwis-metadataportal/openwis-portal/src/main/java/org/openwis/metadataportal/services/system/dto/MaintenanceConfigurationDTO.java
@@ -1,26 +1,46 @@
 package org.openwis.metadataportal.services.system.dto;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class MaintenanceConfigurationDTO {
 
-    private String startDate;
+    private final String datePattern = "yyyy-MM-dd HH:mm";
 
-    private String endDate;
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
 
     private boolean enabled;
 
-    public String getStartDate() {
+    @JsonIgnore
+    public LocalDateTime getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    @JsonProperty("startDate")
+    public String getStartDateAsString() {
+        return startDate.format(DateTimeFormatter.ofPattern(datePattern));
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
         this.startDate = startDate;
     }
 
-    public String getEndDate() {
+    @JsonIgnore
+    public LocalDateTime getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
+    @JsonProperty("endDate")
+    public String getEndDateAsString() {
+        return endDate.format(DateTimeFormatter.ofPattern(datePattern));
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
         this.endDate = endDate;
     }
 
