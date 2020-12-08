36738db801fb8109b288f1b7e135eb204cb83047
Hacene SI HADJ MOHAND <hacene.si-hadj-mohand@akka.eu>
2020-12-08 10:47:41 +0100

    header image home page

--- openwis-metadataportal/openwis-portal/src/main/webapp/scripts/Openwis/lib/Openwis/HomePage/Viewport.js
+++ openwis-metadataportal/openwis-portal/src/main/webapp/scripts/Openwis/lib/Openwis/HomePage/Viewport.js
@@ -235,8 +235,8 @@ Openwis.HomePage.Viewport = Ext.extend(Ext.Viewport, {
 	},
 	getHeaderImage: function(){
 		if(!this.headerImage) {
-			this.headerImage = {tag:'div', cls:'header_img SearchPage', id:'header_img', 
-			html:<div id='urlTitleCont'><div id='urlTitle'>About <strong>ASMC</strong></div><div id='urlSubTitle'>"+Openwis.i18n("HomePage.Main.Header")+"</div></div></a>"}
+			this.headerImage = {tag:'div', cls:'header_img SearchPage', id:'header_img',
+			html: " <div id='urlTitleCont'><div id='urlTitle'><a style=\"text-decoration:none;color:inherit;\" href=\"http://asmc.asean.org/asmc-about/\">About <strong>ASMC</strong></a></div><div id='urlSubTitle'>"+Openwis.i18n("HomePage.Main.Header")+"</div></div>"}
 		}
 		return this.headerImage;
 	},
